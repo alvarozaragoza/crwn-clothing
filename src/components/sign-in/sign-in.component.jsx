ec34d8394757011135468325923d30e118f6a233
Alvaro Zaragoza <alvaro_works@hotmail.com>
2021-11-20 10:08:46 -0300

    Sign-in component improved with Firebase connection and saving data

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -47,7 +47,7 @@ class SignIn extends React.Component {
             value={this.state.password}
             required
           />
-          <div className='buttons' >
+          <div className='buttons'>
             <CustomButton type="submit"> Sign in </CustomButton>
             <CustomButton onClick={signInWithGoogle} isGoogleSignIn> Sign in with Google </CustomButton>
           </div>
